961fe4e52da1dbbaff921b1f40e1ec4fdb7ec706
D4nang <danangutomo6295@gmail.com>
2024-11-06 20:34:25 +0700

    20.34

--- JS/script.js
+++ JS/script.js
@@ -1,41 +1,25 @@
 // Buat Input Fungsinya //
-// 1. Buat Menghitung BMI nya
+// 1. Input data
 function calculateBMI() {
     const height = document.getElementById('height').value / 100; // Konversi ke meter
     const weight = document.getElementById('weight').value;
-
+// 2. Menghitung BMI
     if (height > 0 && weight > 0) {
         const bmi = (weight / (height * height)).toFixed(2);
         let category;
 
         if (bmi < 18.5) {
-            category = 'Kamu kekurangan berat badan';
+            category = 'Kekurangan Berat Badan';
         } else if (bmi >= 18.5 && bmi < 24.9) {
-            category = 'Selamat! kamu udah ideal!';
+            category = 'Berat Badan Ideal';
         } else if (bmi >= 25 && bmi < 29.9) {
-            category = 'Kamu kelebihan berat badan';
+            category = 'Kelebihan Berat Badan';
         } else {
-            category = 'Kamu obesitas';
+            category = 'Obesitas';
         }
-
+// 3. Validasi dan Kalkulasi Hasil
         document.getElementById('result').innerHTML = `BMI Kamu : ${bmi} (${category})`;
     } else {
         alert('Yang bener aja? Kosong dong?');
     }
 }
-
-function showcontent(value) {
-    // Sembunyikan semua konten
-    for (let i = 1; i <= 3; i++) {
-        document.getElementById('content' + i).classList.remove('active');
-    }
-
-    // Tampilkan hanya konten yang dipilih
-    document.getElementById('content' + number).classList.add('active');
-}
-
-const button = document.getElementById('button1');
-
-    button.addEventListener('click', () => {
-        button.style.backgroundColor = '#000000'; // Warna putih jadi hitam saat di klik
-    });
